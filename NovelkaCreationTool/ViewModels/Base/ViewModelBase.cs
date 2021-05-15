5e55413071bc2e934614425c372ca282a79b4ef6
Protanker25 <Protanker25@gmail.com>
2021-05-15 13:02:17 +1000

    добавил класс ViewModelBase

---
+++ NovelkaCreationTool/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace NovelkaCreationTool.ViewModels.Base
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
