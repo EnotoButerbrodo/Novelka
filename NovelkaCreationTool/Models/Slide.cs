16919d4fccf5aea3e8afb05e4f0058c161874545
Protanker25 <Protanker25@gmail.com>
2021-05-15 12:57:34 +1000

    Класс Slide

---
+++ NovelkaCreationTool/Models/Slide.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace NovelkaCreationTool.Models
+{
+    public class Slide
+    {
+        public string Id { get; set; }
+        public BitmapImage Image { get; set; }
+        SlideData Data { get; set; } = new SlideData();
+        public string Speaker
+        {
+            get => Data.Speaker;
+            set => Data.Speaker = value;
+        }
+        public string Text
+        {
+            get => Data.Text;
+            set => Data.Text = value;
+        }
+        public string BackgroundImageName
+        {
+            get => Data.BackgroundImageName;
+            set => Data.BackgroundImageName = value;
+        }
+
+    }
+
+    public class SlideData
+    {
+        public string Speaker { get; set; } = "";
+        public string Text { get; set; } = "";
+        public string BackgroundImageName { get; set; } = "";
+    }
+}
