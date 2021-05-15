9f4c5a3142bb58ddb5c03568ffb9e1019e13643d
Protanker25 <Protanker25@gmail.com>
2021-05-15 13:16:15 +1000

    SlidesViewModel

---
+++ NovelkaCreationTool/ViewModels/SlidesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using NovelkaCreationTool.Models;
+using NovelkaCreationTool.ViewModels.Base;
+
+namespace NovelkaCreationTool.ViewModels
+{
+    public class SlidesViewModel : ViewModelBase
+    {
+        public ObservableCollection<Slide> Slides { get; set; }
+        Slide selectedSlide;
+        public Slide SelectedSlide
+        {
+            get => selectedSlide;
+            set => Set(ref selectedSlide, value);
+        }
+    }
+}
