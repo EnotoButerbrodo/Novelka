76db46f1b8159965884dcf75b47162071a95e770
Protanker25 <Protanker25@gmail.com>
2021-06-10 03:48:20 +1000

    Программа, которая будет воспроизводить новеллы

---
+++ Novelka/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NovelkaCreationTool.ViewModels.Base;
+
+namespace Novelka.ViewModels
+{
+    class MainViewModel:ViewModelBase
+    {
+
+
+    }
+}
