f196c01ae03a9b4277706c329cc79eada3758eec
Protanker25 <Protanker25@gmail.com>
2021-06-19 18:18:49 +1000

    Текстовый блок

---
+++ NovelkaLib/Models/SlideTextBox.cs
@@ -0,0 +1,46 @@
+﻿using NovelkaLib.ViewModels;
+
+namespace NovelkaLib.Models
+{
+    public class SlideTextBox : ViewModelBase
+    {
+        double speakerWidth, speakerHeight,
+               textWidth, textHeight;
+        string imageName;
+        int fontSize;
+
+        public double SpeakerWidth
+        {
+            get => speakerWidth;
+            set => Set(ref speakerWidth, value);
+        }
+        public double SpeakerHeight
+        {
+            get => speakerHeight;
+            set => Set(ref speakerHeight, value);
+        }
+        public double TextWidth
+        {
+            get => textWidth;
+            set => Set(ref textWidth, value);
+        }
+
+        public double TextHeight
+        {
+            get => textHeight;
+            set => Set(ref textHeight, value);
+        }
+        public string ImageName
+        {
+            get => imageName;
+            set => Set(ref imageName, value);
+        }
+
+        public int FontSize
+        {
+            get => fontSize;
+            set => Set(ref fontSize, value);
+        }
+
+    }
+}
